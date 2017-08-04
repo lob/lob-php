b7b85bfcf96dcca09effac9835977503e1b0db0b
Emily Zhang <emily@lob.com>
2017-08-04 10:56:23 -0700

    v2.1.0

--- src/Lob/Lob.php
+++ src/Lob/Lob.php
@@ -39,7 +39,7 @@ class Lob
             $this->setApiKey($apiKey);
         }
         $this->version = $version;
-        $this->clientVersion = '2.0.0';
+        $this->clientVersion = '2.1.0';
     }
 
     public function getApiKey()
