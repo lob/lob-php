7dd85f74dbb90df95ab0f4ee74b68a79a18462b8
Ami Wang <ami@lob.com>
2018-12-05 09:19:27 -0800

    chore(assets): update links

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL | E_STRICT);
 // Ensure that composer has installed all dependencies
 if (!file_exists(dirname(__DIR__) . '/composer.lock')) {
     die("Dependencies must be installed using composer:\n\nphp composer.phar install --dev\n\n"
-        . "See http://getcomposer.org for help with installing composer\n");
+        . "See https://getcomposer.org for help with installing composer\n");
 }
 
 require __DIR__.'/../vendor/autoload.php';
