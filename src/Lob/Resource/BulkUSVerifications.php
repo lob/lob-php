6dba2b24ed5fe0d57e6daaed5cc33db360eb75a4
Amaan Rahim <amaan@lob.com>
2021-08-03 11:07:58 -0700

    Add support for bulk AV endpoints

---
+++ src/Lob/Resource/BulkUSVerifications.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Lob\Resource;
+
+use Lob\ResourceBase;
+use BadMethodCallException;
+
+class BulkUSVerifications extends ResourceBase
+{
+    public function verify(array $data, array $query = array())
+    {
+        return $this->sendRequest(
+            'POST',
+            'bulk/us_verifications',
+            $query,
+            $data
+        );
+    }
+
+    public function all(array $options = array(), $includeMeta = false)
+    {
+        throw new BadMethodCallException(__CLASS__.'::'.__FUNCTION__.'() is not supported.');
+    }
+
+    public function create(array $data, array $headers = null)
+    {
+        throw new BadMethodCallException(__CLASS__.'::'.__FUNCTION__.'() is not supported.');
+    }
+
+    public function get($id)
+    {
+        throw new BadMethodCallException(__CLASS__.'::'.__FUNCTION__.'() is not supported.');
+    }
+
+    public function delete($id)
+    {
+        throw new BadMethodCallException(__CLASS__.'::'.__FUNCTION__.'() is not supported.');
+    }
+}
